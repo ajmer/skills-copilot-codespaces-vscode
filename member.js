740cc9f286c3fc9f873d3fa45e3186398cfdd1b8
Ajmer Dhariwal <ajmer@eraofdata.com>
2023-08-10 20:49:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var skills = document.getElementById("skills").value;
+    var skillsError = document.getElementById("skillsError");
+    var skillsRegex = /^[a-zA-Z0-9]+$/;
+    if (skillsRegex.test(skills) == true) {
+        skillsError.innerHTML = "";
+        return true;
+    } else {
+        skillsError.innerHTML = "Invalid Skills";
+        return false;
+    }
+}
